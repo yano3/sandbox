cd4c056d8741fe5f8255d6c4953d9e9b493a79ad
Teppei Yano <buckelwal@gmail.com>
2021-07-07 12:56:38 +0900

    show version

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"runtime/debug"
 )
 
+var version = ""
+
 func main() {
-	fmt.Println("sandbox")
+	fmt.Printf("sandbox version %s\n", getVersion())
+}
+
+func getVersion() string {
+	if version != "" {
+		return version
+	}
+
+	i, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "(unknown)"
+	}
+	return i.Main.Version
 }
